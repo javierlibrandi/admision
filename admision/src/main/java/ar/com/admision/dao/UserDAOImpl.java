cfe25ff5edf05f7ad6495024cbd3a5a7bde4bdd9
javierlibrandi <javierlibrandi@gmail.com>
2014-07-22 17:48:38 -0300

    Comienzo la modificacion para usar JPA

--- admision/src/main/java/ar/com/admision/dao/UserDAOImpl.java
+++ admision/src/main/java/ar/com/admision/dao/UserDAOImpl.java
@@ -1,28 +1,37 @@
 package ar.com.admision.dao;
 
-import org.hibernate.Query;
+import java.io.Serializable;
+
+
+
+
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
 import ar.com.admision.dominio.User;
 
 @Repository
-public class UserDAOImpl extends AbstractDAO<User> implements UserDAO{
+public class UserDAOImpl extends AbstractDAO<User,Serializable> implements UserDAO{
 
 	public User findByUserNamePassword(String userName, String password) {
 		String strQuery = "from User u where u.userName = :userName and " + 
 						  "password = :password";
+		Query query = em.createQuery(strQuery);
 		
-		Query query = currentSession().createQuery(strQuery);
+		//Query query = currentSession().createQuery(strQuery);
 		
 		query.setParameter("userName", userName)
 		  	 .setParameter("password", password);
 		
-		return (User) query.uniqueResult();
+		return (User) query.getSingleResult();
 	}
 
 	public Class<User> getEntityClass() {
 		return User.class;
 	}
+
+
 	
 	
 }
